ef26a1ca881d449b8960cd73135f6c7351802b2b
Максим <sinelnikov-web@mail.ru>
2023-07-08 00:25:43 +0600

    optimization #3

--- src/index.js
+++ src/index.js
@@ -338,19 +338,25 @@ function Main() {
             </div>
 
             <div className="section__panel-wrapper" ref={ref}>
-                {TABS_KEYS.map(key =>
-                    <div key={key} role="tabpanel" className={'section__panel' + (key === activeTab ? '' : ' section__panel_hidden')} aria-hidden={key === activeTab ? 'false' : 'true'} id={`panel_${key}`} aria-labelledby={`tab_${key}`}>
-                        <ul className="section__panel-list">
-                            {TABS[key].items.map((item, index) =>
-                                <Event
-                                    key={index}
-                                    {...item}
-                                    onSize={onSize}
-                                />
-                            )}
-                        </ul>
-                    </div>
-                )}
+                {TABS_KEYS.map(key => (
+                    <>
+                        {key === activeTab ? (
+                            <div key={key} role="tabpanel" className={'section__panel'} aria-hidden={'false'} id={`panel_${key}`} aria-labelledby={`tab_${key}`}>
+                                <ul className="section__panel-list">
+                                    {TABS[key].items.map((item, index) =>
+                                        <Event
+                                            key={index}
+                                            {...item}
+                                            onSize={onSize}
+                                        />
+                                    )}
+                                </ul>
+                            </div>
+                        ) : (
+                            <></>
+                        )}
+                    </>
+                ))}
                 {hasRightScroll &&
                     <div className="section__arrow" onClick={onArrowCLick}></div>
                 }
